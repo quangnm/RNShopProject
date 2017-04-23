b55007b41d8ab12eb675c57675a2c77a1364c9eb
mssolutions <mstek030417@gmail.com>
2017-04-23 11:16:47 +0700

    Xong giao dien Header

--- src/components/Main/Shop/Header.js
+++ src/components/Main/Shop/Header.js
@@ -4,21 +4,70 @@ import {
     Text,
     TouchableOpacity,
     Dimensions,
+    Image,
+    StyleSheet,
+    TextInput,
 } from 'react-native';
+
+import icMenu from '../../Images/appIcon/ic_menu.png';
+import icLogo from '../../Images/appIcon/ic_logo.png';
 
 const { height } = Dimensions.get('window');
 
 export default class Shop extends Component {
     render() {
+        const {
+            container,
+            topContainer,
+            titleStyle,
+            textInputStyle,
+            iconStyle,
+        } = styles;
+
         return (
-            <View style={{ height: height / 8 }}>
-                <TouchableOpacity
-                    onPress={this.props.onOpenMenu}
-                >
-                    <Text>Menu</Text>
-                </TouchableOpacity>
-            </View>
+            <View style={container}>
+                <View style={topContainer} >
+                    <TouchableOpacity
+                        onPress={this.props.onOpenMenu}
+                    >
+                        <Image source={icMenu} style={iconStyle} />
+                    </TouchableOpacity>
+                    <Text style={titleStyle}>Dress Shopping</Text>
+                    <Image source={icLogo} style={iconStyle} />
+                </View>
+                <TextInput
+                    style={textInputStyle}
+                    placeholder='What do you want to buy?'
+                />
+            </View >
         );
     }
 }
 
+const styles = StyleSheet.create({
+    container: {
+        height: height / 8,
+        backgroundColor: '#34B089',
+        padding: 10,
+        justifyContent: 'space-around'
+    },
+    topContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    textInputStyle: {
+        backgroundColor: '#FFFFFF',
+        height: height / 22,
+        padding: 5,
+        marginTop: 10,
+    },
+    titleStyle: {
+        color: '#FFFFFF',
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    iconStyle: {
+        width: 27,
+        height: 27,
+    }
+});
