dc1b73ef4ffc7b06b3897ad746475d235ff27af0
mssolutions <mstek030417@gmail.com>
2017-04-23 12:41:03 +0700

    Xong: giao dien tabbar

--- src/components/Main/Shop/Shop.js
+++ src/components/Main/Shop/Shop.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {
     View,
-
+    Image,
+    StyleSheet,
 } from 'react-native';
 import TabNavigator from 'react-native-tab-navigator';
 import Home from './Home/Home.js';
@@ -9,6 +10,15 @@ import Contact from './Contact/Contact.js';
 import Cart from './Cart/Cart.js';
 import Search from './Search/Search.js';
 import Header from './Header.js';
+
+import icHomeSelected from '../../Images/appIcon/home.png';
+import icHomeUnSelected from '../../Images/appIcon/home0.png';
+import icSearchSelected from '../../Images/appIcon/search.png';
+import icSearchUnSelected from '../../Images/appIcon/search0.png';
+import icCartSelected from '../../Images/appIcon/cart.png';
+import icCartUnSelected from '../../Images/appIcon/cart0.png';
+import icContactSelected from '../../Images/appIcon/contact.png';
+import icContactUnSelected from '../../Images/appIcon/contact0.png';
 
 export default class Shop extends Component {
     constructor(props) {
@@ -24,6 +34,9 @@ export default class Shop extends Component {
     }
 
     render() {
+        const {
+            iconStyle,
+        } = styles;
         return (
             <View style={{ flex: 1 }}>
                 <Header onOpenMenu={this.openMenu.bind(this)} />
@@ -31,28 +44,49 @@ export default class Shop extends Component {
                     <TabNavigator.Item
                         selected={this.state.selectedTab === 'home'}
                         title="Home"
+                        renderIcon={() => <Image source={icHomeUnSelected} style={iconStyle} />}
+                        renderSelectedIcon={() => <Image
+                            source={icHomeSelected} style={iconStyle}
+                        />}
                         onPress={() => this.setState({ selectedTab: 'home' })}
+                        selectedTitleStyle={{ color: '#34B089' }}
                     >
                         <Home />
                     </TabNavigator.Item>
                     <TabNavigator.Item
                         selected={this.state.selectedTab === 'search'}
                         title="Search"
+                        renderIcon={() => <Image source={icSearchUnSelected} style={iconStyle} />}
+                        renderSelectedIcon={() => <Image
+                            source={icSearchSelected} style={iconStyle}
+                        />}
                         onPress={() => this.setState({ selectedTab: 'search' })}
+                        selectedTitleStyle={{ color: '#34B089' }}
                     >
                         <Search />
                     </TabNavigator.Item>
                     <TabNavigator.Item
                         selected={this.state.selectedTab === 'cart'}
                         title="Cart"
+                        renderIcon={() => <Image source={icCartUnSelected} style={iconStyle} />}
+                        renderSelectedIcon={() => <Image
+                            source={icCartSelected} style={iconStyle}
+                        />}
+                        badgeText='10'
                         onPress={() => this.setState({ selectedTab: 'cart' })}
+                        selectedTitleStyle={{ color: '#34B089' }}
                     >
                         <Cart />
                     </TabNavigator.Item>
                     <TabNavigator.Item
                         selected={this.state.selectedTab === 'contact'}
                         title="Contact"
+                        renderIcon={() => <Image source={icContactUnSelected} style={iconStyle} />}
+                        renderSelectedIcon={() => <Image
+                            source={icContactSelected} style={iconStyle}
+                        />}
                         onPress={() => this.setState({ selectedTab: 'contact' })}
+                        selectedTitleStyle={{ color: '#34B089' }}
                     >
                         <Contact />
                     </TabNavigator.Item>
@@ -61,3 +95,10 @@ export default class Shop extends Component {
         );
     }
 }
+
+const styles = StyleSheet.create({
+    iconStyle: {
+        width: 25,
+        height: 25,
+    },
+});
