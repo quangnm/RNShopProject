1b08cf0a1506daf93b97d117f2dae074c9c9d447
mssolutions <mstek030417@gmail.com>
2017-04-26 10:16:54 +0700

    Hoan thanh giao dien menu

--- src/components/Main/Menu.js
+++ src/components/Main/Menu.js
@@ -4,9 +4,19 @@ import {
     Text,
     StyleSheet,
     TouchableOpacity,
+    Image,
 } from 'react-native';
 
+import profileImage from '../Images/temp/profile.png';
+
 export default class Menu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLogedIn: false,
+        };
+    }
+
     gotoAuthentication() {
         const navigator = this.props.navigator;
         navigator.push({ name: 'AUTHENTICATION' });
@@ -23,38 +33,89 @@ export default class Menu extends Component {
     }
 
     render() {
+        const {
+            container,
+            // buttonContainer,
+            // buttonTitle,
+            // buttonBlue,
+            // buttonGreen,
+            // buttonRed,
+            profileImageContainer,
+            buttonStyle,
+            buttonSignInStyle,
+            buttonSignOutStyle,
+            titleButtonStyle,
+            userNameStyle,
+            userNameContainer,
+        } = styles;
+
+        //Co the: sau nay se chuyen thanh 1 component rieng
+        const logoutJSX = (
+            <View style={{ flex: 1 }}>
+                <TouchableOpacity style={[buttonStyle, buttonSignInStyle]}>
+                    <Text style={titleButtonStyle}>Sign In</Text>
+                </TouchableOpacity>
+            </View>
+        );
+
+        const loginJSX = (
+            <View style={{ flex: 1 }}>
+                <View style={userNameContainer}>
+                    <Text style={userNameStyle}>User name</Text>
+                </View>
+                <TouchableOpacity style={[buttonStyle, buttonSignOutStyle]}>
+                    <Text style={titleButtonStyle}>Order History</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[buttonStyle, buttonSignOutStyle]}>
+                    <Text style={titleButtonStyle}>Change Info</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[buttonStyle, buttonSignOutStyle]}>
+                    <Text style={titleButtonStyle}>Log out</Text>
+                </TouchableOpacity>
+            </View>
+        );
+
+        const mainJSX = this.state.isLogedIn ? loginJSX : logoutJSX;
+
         return (
-            <View style={styles.container}>
-                <Text>Menu</Text>
-                <TouchableOpacity
-                    style={[styles.buttonContainer, styles.buttonRed]}
-                    onPress={this.gotoAuthentication.bind(this)}
-                >
-                    <Text style={styles.buttonTitle}>Go to Authentication</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={[styles.buttonContainer, styles.buttonBlue]}
-                    onPress={this.gotoChangeInfo.bind(this)}
-                >
-                    <Text style={styles.buttonTitle}>Go to ChangeInfo</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={[styles.buttonContainer, styles.buttonGreen]}
-                    onPress={this.gotoOrderHistory.bind(this)}
-                >
-                    <Text style={styles.buttonTitle}>Go to OrderHistory</Text>
-                </TouchableOpacity>
+            <View style={container}>
+                <Image
+                    source={profileImage}
+                    style={profileImageContainer}
+                />
+                {mainJSX}
             </View>
         );
     }
 }
+/*
+<TouchableOpacity
+    style={[buttonContainer, buttonRed]}
+    onPress={this.gotoAuthentication.bind(this)}
+>
+    <Text style={buttonTitle}>Go to Authentication</Text>
+</TouchableOpacity>
+<TouchableOpacity
+    style={[buttonContainer, buttonBlue]}
+    onPress={this.gotoChangeInfo.bind(this)}
+>
+    <Text style={buttonTitle}>Go to ChangeInfo</Text>
+</TouchableOpacity>
+<TouchableOpacity
+    style={[buttonContainer, buttonGreen]}
+    onPress={this.gotoOrderHistory.bind(this)}
+>
+    <Text style={buttonTitle}>Go to OrderHistory</Text>
+</TouchableOpacity>
+*/
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'gray'
+        backgroundColor: '#34B089',
+        borderRightWidth: 3,
+        borderRightColor: '#FFFFFF',
     },
     buttonContainer: {
         width: 200,
@@ -77,5 +138,41 @@ const styles = StyleSheet.create({
         color: 'white',
         padding: 10,
         fontWeight: 'bold'
+    },
+    profileImageContainer: {
+        width: 140,
+        height: 140,
+        borderRadius: 70,
+        marginBottom: 30,
+        marginTop: 10,
+    },
+    buttonStyle: {
+        height: 44,
+        borderRadius: 5,
+        backgroundColor: '#ffffff',
+        justifyContent: 'center',
+        marginBottom: 10
+    },
+    buttonSignInStyle: {
+        alignItems: 'center',
+        paddingHorizontal: 90,
+    },
+    buttonSignOutStyle: {
+        paddingHorizontal: 130,
+        paddingLeft: 10,
+    },
+    titleButtonStyle: {
+        color: '#34B089',
+        fontSize: 15,
+    },
+    userNameStyle: {
+        color: '#ffffff',
+        marginBottom: 20,
+        fontSize: 20,
+        fontWeight: '600'
+    },
+    userNameContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
     },
 });
